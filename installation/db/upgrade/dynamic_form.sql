7f14cb42bd56e7a7578c25c9808ff05d7d9f694f
Cornelius Hansjakob <cornelius.h@gmx.at>
2012-02-03 13:03:44 +0100

    sql for dynforms

--- installation/db/upgrade/dynamic_form.sql
+++ installation/db/upgrade/dynamic_form.sql
@@ -14,29 +14,10 @@ CREATE TABLE IF NOT EXISTS `pageDynForm` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
--- Tabellenstruktur für Tabelle `page-DEFAULT_FORM-1-Region100-Instances`
---
-
-CREATE TABLE IF NOT EXISTS `page-DEFAULT_FORM-1-Region100-Instances` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `pageId` varchar(32) NOT NULL,
-  `version` int(10) unsigned NOT NULL,
-  `idLanguages` int(10) unsigned NOT NULL DEFAULT '1',
-  `sortPosition` int(10) unsigned NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `field_type` bigint(20) NOT NULL,
-  `mandatory` bigint(20) NOT NULL,
-  `validation` bigint(20) NOT NULL,
-  `display` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `pageId` (`pageId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT AUTO_INCREMENT=1 ;
-
---
 -- Tabellenstruktur für Tabelle `page-DEFAULT_FORM-1-Region100-InstanceMultiFields`
 --
 
+DROP TABLE IF EXISTS `page-DEFAULT_FORM-1-Region100-InstanceMultiFields`;
 CREATE TABLE IF NOT EXISTS `page-DEFAULT_FORM-1-Region100-InstanceMultiFields` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `pageId` varchar(32) NOT NULL,
@@ -51,13 +32,41 @@ CREATE TABLE IF NOT EXISTS `page-DEFAULT_FORM-1-Region100-InstanceMultiFields` (
   KEY `idRegionInstances` (`idRegionInstances`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
 --
--- Constraints der exportierten Tabellen
+-- Tabellenstruktur für Tabelle `page-DEFAULT_FORM-1-Region100-Instances`
 --
 
-ALTER TABLE `page-DEFAULT_FORM-1-Region100-Instances` 
-  ADD FOREIGN KEY ( `pageId` ) REFERENCES `pages` (`pageId`) ON DELETE CASCADE ;
+DROP TABLE IF EXISTS `page-DEFAULT_FORM-1-Region100-Instances`;
+CREATE TABLE IF NOT EXISTS `page-DEFAULT_FORM-1-Region100-Instances` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `pageId` varchar(32) NOT NULL,
+  `version` int(10) unsigned NOT NULL,
+  `idLanguages` int(10) unsigned NOT NULL DEFAULT '1',
+  `sortPosition` int(10) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `field_type` bigint(20) NOT NULL,
+  `mandatory` bigint(20) NOT NULL,
+  `validation` bigint(20) NOT NULL,
+  `maxlength` int(11) DEFAULT NULL,
+  `display` bigint(20) NOT NULL,
+  `other` bigint(20) NOT NULL,
+  `options` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pageId` (`pageId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT AUTO_INCREMENT=1 ;
 
+--
+-- Constraints der Tabelle `page-DEFAULT_FORM-1-Region100-InstanceMultiFields`
+--
 ALTER TABLE `page-DEFAULT_FORM-1-Region100-InstanceMultiFields`
-  ADD FOREIGN KEY (`idRegionInstances`) REFERENCES `page-DEFAULT_FORM-1-Region100-Instances` (`id`) ON DELETE CASCADE,
-  ADD FOREIGN KEY (`pageId`) REFERENCES `pages` (`pageId`) ON DELETE CASCADE;
+  ADD CONSTRAINT `page-DEFAULT_FORM-1-Region100-InstanceMultiFields_ibfk_1` FOREIGN KEY (`idRegionInstances`) REFERENCES `page-DEFAULT_FORM-1-Region100-Instances` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `page-DEFAULT_FORM-1-Region100-InstanceMultiFields_ibfk_2` FOREIGN KEY (`pageId`) REFERENCES `pages` (`pageId`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `page-DEFAULT_FORM-1-Region100-Instances`
+--
+ALTER TABLE `page-DEFAULT_FORM-1-Region100-Instances`
+  ADD CONSTRAINT `page-DEFAULT_FORM-1-Region100-Instances_ibfk_1` FOREIGN KEY (`pageId`) REFERENCES `pages` (`pageId`) ON DELETE CASCADE;
