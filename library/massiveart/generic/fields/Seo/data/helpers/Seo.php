0b1eedbe7a12d4d0e7763dd5fbdb04a43f912071
NurbekChymbaev <nurbek188@gmail.com>
2012-09-17 00:36:15 +0600

    add 'Seo' field type files

---
+++ library/massiveart/generic/fields/Seo/data/helpers/Seo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Nurbek
+ * Date: 9/16/12
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class GenericDataHelper_Seo extends GenericDataHelperAbstract
+{
+
+}
