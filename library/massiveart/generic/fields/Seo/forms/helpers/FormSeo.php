4a9b1a5a7b82493272b20e418388ab86b1471bb4
NurbekChymbaev <nurbek188@gmail.com>
2012-09-17 00:36:15 +0600

    add 'Seo' field type files

---
+++ library/massiveart/generic/fields/Seo/forms/helpers/FormSeo.php
@@ -0,0 +1,10 @@
+<?php
+
+class Form_Helper_FormSeo extends Zend_View_Helper_FormElement
+{
+    function formSeo($name, $value = null, $attribs = null)
+    {
+        return 'New Seo Field Type';
+    }
+
+}
