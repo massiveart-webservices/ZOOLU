51cf226bcc52aa42648a0703d0a7bc61142ba915
Raphael Stocker <raphael.stocker@massiveart.com>
2012-09-27 11:05:03 +0200

    BUGFIX sortposition of pages by loading overview with subsites

--- application/zoolu/modules/cms/models/Pages.php
+++ application/zoolu/modules/cms/models/Pages.php
@@ -784,7 +784,7 @@ class Model_Pages extends ModelAbstract
         if ($intSortTypeId > 0 && $intSortTypeId != '') {
             switch ($intSortTypeId) {
                 case $this->core->sysConfig->sort->types->manual_sort->id:
-                    $strSqlOrderBy = ' ORDER BY sortPosition ' . $strSortOrder . ', sortTimestamp ' . (($strSortOrder == 'DESC') ? 'ASC' : 'DESC');
+                    $strSqlOrderBy = ' ORDER BY folderSortPosition '.$strSortOrder.', folderSortTimestamp '.$strSortOrder.', sortPosition '.$strSortOrder.', sortTimestamp '.(($strSortOrder == 'DESC') ? 'DESC' : 'DESC');
                     break;
                 case $this->core->sysConfig->sort->types->created->id:
                     $strSqlOrderBy = ' ORDER BY created ' . $strSortOrder;
@@ -881,12 +881,12 @@ class Model_Pages extends ModelAbstract
         }
 
         $sqlStmt = $this->core->dbh->query('SELECT DISTINCT id, plId, genericFormId, version, plGenericFormId, plVersion,
-                                          url, plUrl, title, languageCode, idPageTypes, idDestination, hideInSitemap, sortPosition, sortTimestamp, created, changed, published, target
+                                          url, plUrl, title, languageCode, idPageTypes, folderSortPosition, folderSortTimestamp, idDestination, hideInSitemap, sortPosition, sortTimestamp, created, changed, published, target
                                         FROM
                                           (SELECT pages.id, pl.id AS plId, genericForms.genericFormId, genericForms.version,
                                             plGenForm.genericFormId AS plGenericFormId, plGenForm.version AS plVersion, urls.url, lUrls.url AS plUrl, 
                                             IF(pageProperties.idPageTypes = ?, plTitle.title, pageTitles.title) as title, languageCode, pageProperties.idPageTypes, pageProperties.idDestination, pageProperties.hideInSitemap,
-                                            pageProperties.created, pageProperties.changed, pageProperties.published, folders.sortPosition, folders.sortTimestamp, pageTargets.target
+                                            pageProperties.created, pageProperties.changed, pageProperties.published, folders.sortPosition AS folderSortPosition, folders.sortTimestamp AS folderSortTimestamp, pageTargets.target
                                           FROM folders
                                           	INNER JOIN folderProperties ON
                                           	  folderProperties.folderId = folders.folderId AND
@@ -970,7 +970,7 @@ class Model_Pages extends ModelAbstract
                                           SELECT pages.id, pl.id AS plId, genericForms.genericFormId, genericForms.version,
                                             plGenForm.genericFormId AS plGenericFormId, plGenForm.version AS plVersion, urls.url, lUrls.url AS plUrl,
                                             IF(pageProperties.idPageTypes = ?, plTitle.title, pageTitles.title) as title, languageCode, pageProperties.idPageTypes, pageProperties.idDestination, pageProperties.hideInSitemap,
-                                            pageProperties.created, pageProperties.changed, pageProperties.published, pages.sortPosition, pages.sortTimestamp, pageTargets.target                                            
+                                            pageProperties.created, pageProperties.changed, pageProperties.published, NULL AS folderSortPosition, NULL AS folderSortTimestamp, pages.sortPosition, pages.sortTimestamp, pageTargets.target                                            
                                           FROM pages
                                             INNER JOIN pageProperties ON 
                                               pageProperties.pageId = pages.pageId AND 
---
+++ public/website/themes/default/js_incs/modus.js
@@ -0,0 +1,9 @@
+function showModusContainer(){
+    var toolbar = document.getElementById('zoolu-modus-toolbar');
+    toolbar.style.top = '0px';
+};
+
+function hideModusContainer(){
+    var toolbar = document.getElementById('zoolu-modus-toolbar');
+    toolbar.style.top = '-40px';
+};
