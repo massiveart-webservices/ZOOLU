cbc1b2b54ffb2737d9ce3d259f93976e26b74e6e
NurbekChymbaev <nurbek188@gmail.com>
2012-10-15 19:18:03 +0600

    get seo url from #page_url

--- public/zoolu-statics/js_incs/form.js
+++ public/zoolu-statics/js_incs/form.js
@@ -1929,7 +1929,7 @@ Massiveart.Form = Class.create({
 
     this.updateSnippetPreviewTitle();
     this.updateSnippetPreviewDesc();
-    this.updateSnippetPreviewUrl(true);
+    this.updateSnippetPreviewUrl();
 
     $('articletitle').observe('keyup', function(event){
         myForm.updateSnippetPreviewTitle();
@@ -1946,7 +1946,7 @@ Massiveart.Form = Class.create({
     $('seo_keywords').observe('keyup', function(event) {
         myForm.updateSnippetPreviewTitle();
         myForm.updateSnippetPreviewDesc();
-        myForm.updateSnippetPreviewUrl(false);
+        myForm.updateSnippetPreviewUrl();
     });
   },
 
@@ -1988,11 +1988,9 @@ Massiveart.Form = Class.create({
     $('snippet_seo_desc').update( desc );
   },
 
-  updateSnippetPreviewUrl: function ( onload ) {
-    var snippet_url = $('snippet_seo_url').innerHTML;
-    if( onload ) {
-        snippet_url += $('page_url').readAttribute('href');
-    }
+  updateSnippetPreviewUrl: function () {
+    var snippet_url = $('page_url').readAttribute('href');
+    snippet_url = snippet_url.replace('http://', '');
     snippet_url = this.pickOutSeoKeywords(snippet_url, true);
     $('snippet_seo_url').innerHTML = snippet_url;
   },
