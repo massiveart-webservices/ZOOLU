e7b60af69408adf920f1227e7a5cde0b2013151f
NurbekChymbaev <nurbek188@gmail.com>
2012-10-18 20:36:30 +0600

    update tables by `name` instead `id`

--- installation/db/upgrade/20120920_seo_fieldtype.sql
+++ installation/db/upgrade/20120920_seo_fieldtype.sql
@@ -2,19 +2,16 @@
 INSERT INTO `fieldTypes` (`id`, `idDecorator`, `sqlType`, `size`, `title`, `defaultValue`, `idFieldTypeGroup`) VALUES ('35', '1', '', '0', 'seo', '', '5');
 
 -- update seo_description, seo_keywords, seo_title, seo_canonical fields types to `seo`
-UPDATE `fields` SET `idFieldTypes` = '35' WHERE `fields`.`id` = 247;
-UPDATE `fields` SET `idFieldTypes` = '35' WHERE `fields`.`id` = 248;
-UPDATE `fields` SET `idFieldTypes` = '35' WHERE `fields`.`id` = 249;
-UPDATE `fields` SET `idFieldTypes` = '35' WHERE `fields`.`id` = 250;
+UPDATE `fields` SET `idFieldTypes` = '35' WHERE `name` = 'seo_description';
+UPDATE `fields` SET `idFieldTypes` = '35' WHERE `name` = 'seo_keywords';
+UPDATE `fields` SET `idFieldTypes` = '35' WHERE `name` = 'seo_title';
+UPDATE `fields` SET `idFieldTypes` = '35' WHERE `name` = 'seo_canonical';
 
 -- update seo fields `options`
-UPDATE `fields` SET `options` = '{"textbox":"textarea", "seoname":"meta description", "charslimit":"156"}'
-                                WHERE `fields`.`id` = 247;
-UPDATE `fields` SET `options` = '{"textbox":"textarea", "seoname":"meta keywords", "charslimit":"156"}'
-                                WHERE `fields`.`id` =248;
-UPDATE `fields` SET `options` = '{"textbox":"textarea", "seoname":"meta keywords", "charslimit":"156"}'
-                                WHERE `fields`.`id` =249;
-UPDATE `fields` SET `options` = '{"textbox":"text", "seoname":"page title", "charslimit":"70"}'
-                                WHERE `fields`.`id` =250;
-UPDATE `fields` SET `options` = '{"textbox":"text", "seoname":"canonical page", "charslimit":"100"}'
-                                WHERE `fields`.`id` =251;
+UPDATE `fields` SET `options` = '{"textbox":"textarea", "seoname":"meta description", "charslimit":"156"}' WHERE `name` = 'seo_description';
+
+UPDATE `fields` SET `options` = '{"textbox":"textarea", "seoname":"meta keywords", "charslimit":"156"}' WHERE `name` = 'seo_keywords';
+
+UPDATE `fields` SET `options` = '{"textbox":"text", "seoname":"page title", "charslimit":"70"}' WHERE `name` = 'seo_title';
+
+UPDATE `fields` SET `options` = '{"textbox":"text", "seoname":"canonical page", "charslimit":"100"}' WHERE `name` = 'seo_canonical';
