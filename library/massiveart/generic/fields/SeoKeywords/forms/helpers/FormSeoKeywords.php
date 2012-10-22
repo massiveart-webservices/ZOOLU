a54f4fab2c6a6333a87fa41a9be1783a6158e0ae
NurbekChymbaev <nurbek188@gmail.com>
2012-10-22 16:11:30 +0600

    show keywords count under seo keywords

--- library/massiveart/generic/fields/SeoKeywords/forms/helpers/FormSeoKeywords.php
+++ library/massiveart/generic/fields/SeoKeywords/forms/helpers/FormSeoKeywords.php
@@ -8,14 +8,20 @@ class Form_Helper_FormSeoKeywords extends Zend_View_Helper_FormElement
         $info = $this->_getInfo($name, $value, $attribs);
         extract($info);
 
-        $strOutput = '';
+        if( $value == '' ) {
+            $keywords_count = 0;
+        } else {
+            $keywords_count = count(explode(',', $value));
+        }
 
+        $strOutput = '';
         $strOutput .= '<textarea rows="80" cols="24" name="'.$name.'" id="'.$name.'"
                                 onKeyDown="myForm.limitKeywords(\''.$name.'\', '.$max_keywords.')"
                                 onKeyUp="myForm.limitKeywords(\''.$name.'\', '.$max_keywords.')"
                            >'
                             . $this->view->escape($value) .
                       '</textarea>';
+        $strOutput .= 'Keywords count: <span id="seo_keywords_count" class="plus">'.$keywords_count.'</span>';
 
         return $strOutput;
     }
