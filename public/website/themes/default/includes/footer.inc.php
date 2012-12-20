8d76722c40d5176466603350cce237ac51cfc997
mob8607 <mathias.ober@massiveart.com>
2012-12-20 17:40:25 +0100

    added helpers and includes

---
+++ public/website/themes/default/includes/footer.inc.php
@@ -0,0 +1,9 @@
+    <footer id="footer">
+        <div class="inner">
+            <div class="left">&copy; <?php echo date('Y'); ?> Firmenname</div>
+            <div class="right">
+                <?php get_footer_navigation(); ?>          
+            </div>
+            <div class="clear"></div>
+        </div>      
+    </footer> <!-- /#footer -->
---
+++ public/website/themes/default/includes/header.inc.php
@@ -0,0 +1,19 @@
+    <!-- Header -->
+    <header class="header">
+        <div class="inner">
+            <div class="logo">Logospace</div>
+            <div class="slogan">Slogan Platzhalter</div>
+            <div class="search">
+                <input type="text" name="iptSearch" id="iptSearch" value="Suche"/>
+            </div>
+        </div>
+    </header> <!-- /.header -->
+    <!-- Navigation -->
+    <nav class="nav">
+        <div class="inner">
+            <ul>
+                <?php get_main_navigation('li', '', 'selected', true, false); ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+    </nav> <!-- /.nav -->
