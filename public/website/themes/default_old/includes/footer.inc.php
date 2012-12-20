85bbf37bf2f31f99ec20d00275f546776be31d6c
rs2487 <raphael.stocker@massiveart.com>
2012-12-20 09:47:02 +0100

    Init for dev 20121220

--- public/website/themes/default_old/includes/footer.inc.php
+++
@@ -1,9 +0,0 @@
-  <div id="footer">
-    <div class="inner">
-      <div class="left">&copy; <?php echo date('Y'); ?> Firmenname</div>
-      <div class="right">
-        <?php get_footer_navigation(); ?>          
-      </div>
-      <div class="clear"></div>
-    </div>      
-  </div> <!-- /#footer -->
