d7928fe2807fdd3fb73bd9a6b0a28f24a3280c7c
mob8607 <mathias.ober@massiveart.com>
2012-12-20 08:42:40 +0100

    moved default theme in default_old

--- public/website/themes/default/includes/subnavigation.inc.php
+++
@@ -1,6 +0,0 @@
-        <?php if(has_sub_navigation()) : ?>      
-          <ul>
-            <?php get_sub_navigation(); ?>               
-          </ul>
-        <?php endif; ?>          
-      
