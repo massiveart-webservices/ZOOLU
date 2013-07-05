927bb6061b1a75474eff9a90381f4c5241f34d18
L91 <alexander.schranz@massiveart.com>
2013-07-05 14:47:12 +0200

    fix folder security permissions

--- library/massiveart/website/navigation.class.php
+++ library/massiveart/website/navigation.class.php
@@ -893,12 +893,20 @@ class Navigation
     public function checkZonePrivileges()
     {
         $blnAuthorized = true;
-
+        
         Security::get()->addFoldersToAcl($this->getModelFolders(), Security::ZONE_WEBSITE);
+        Security::get('CustomerSecurity')->addFoldersToAcl($this->getModelFolders(), Security::ZONE_WEBSITE);
 
         if (count($this->ParentFolders()) > 0) {
             foreach ($this->ParentFolders() as $objParentFolderData) {
-                if ($objParentFolderData->isSecure == 1 && !Security::get()->isAllowed(Security::RESOURCE_FOLDER_PREFIX . $objParentFolderData->id, Security::PRIVILEGE_VIEW, true, false, Security::ZONE_WEBSITE) && !Security::get()->isAllowed(Security::RESOURCE_FOLDER_PREFIX . $objParentFolderData->id . '_' . $this->intLanguageId, Security::PRIVILEGE_VIEW, true, false, Security::ZONE_WEBSITE)) {
+                if ($objParentFolderData->isSecure == 1 && (
+                        (
+                            !Security::get()->isAllowed(Security::RESOURCE_FOLDER_PREFIX . $objParentFolderData->id, Security::PRIVILEGE_VIEW, true, false, Security::ZONE_WEBSITE) && !Security::get()->isAllowed(Security::RESOURCE_FOLDER_PREFIX . $objParentFolderData->id . '_' . $this->intLanguageId, Security::PRIVILEGE_VIEW, true, false, Security::ZONE_WEBSITE)
+                        ) ||
+                        (
+                            !Security::get('CustomerSecurity')->isAllowed(Security::RESOURCE_FOLDER_PREFIX . $objParentFolderData->id, Security::PRIVILEGE_VIEW, true, false, Security::ZONE_WEBSITE) && !Security::get('CustomerSecurity')->isAllowed(Security::RESOURCE_FOLDER_PREFIX . $objParentFolderData->id . '_' . $this->intLanguageId, Security::PRIVILEGE_VIEW, true, false, Security::ZONE_WEBSITE)
+                        )
+                    )) {
                     $blnAuthorized = false;
                     break;
                 }
