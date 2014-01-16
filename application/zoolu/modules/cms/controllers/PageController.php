77c80ba965aa7fb53bcab72d5c13734020f5454c
rs2487 <raphael.stocker@massiveart.com>
2014-01-16 16:23:58 +0100

    Fixed Permissions bug #221 by checking permissions more detailed on rootlevel and language

--- application/zoolu/modules/cms/controllers/PageController.php
+++ application/zoolu/modules/cms/controllers/PageController.php
@@ -710,6 +710,8 @@ class Cms_PageController extends AuthControllerAction
                 $this->view->pageurl = $strProtocol . '://' . $strBaseUrl . $this->objForm->Setup()->getField('url')->getValue();
             }
 
+            $this->view->languageOptions = HtmlOutput::getOptionsOfSQL($this->core, 'SELECT languages.id AS VALUE, languages.languageCode AS DISPLAY FROM languages INNER JOIN rootLevelLanguages ON rootLevelLanguages.idLanguages = languages.id AND rootLevelLanguages.idRootLevels = ' . $this->objForm->Setup()->getRootLevelId() . ' ORDER BY languages.sortOrder, languages.languageCode', $this->objForm->Setup()->getLanguageId());
+
             $blnGeneralDeleteAuthorization = Security::get()->isAllowed(Security::RESOURCE_ROOT_LEVEL_PREFIX . $this->objForm->Setup()->getRootLevelId(), Security::PRIVILEGE_DELETE, false, false);
             $blnGeneralUpdateAuthorization = Security::get()->isAllowed(Security::RESOURCE_ROOT_LEVEL_PREFIX . $this->objForm->Setup()->getRootLevelId(), Security::PRIVILEGE_UPDATE, false, false);
 
