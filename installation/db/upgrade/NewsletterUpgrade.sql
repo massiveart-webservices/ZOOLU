5763a9acc31d05f7b30e91c28b06336aaea8aae4
L91 <alexander.schranz@massiveart.com>
2014-06-06 10:43:02 +0200

    fname sname not required in newsletter

---
+++ installation/db/upgrade/NewsletterUpgrade.sql
@@ -0,0 +1,3 @@
+/* Fname Sname no keyfields */
+UPDATE  `zo-zoolu`.`fields` SET  `isKeyField` =  '0' WHERE  `fields`.`id` =224;
+UPDATE  `zo-zoolu`.`fields` SET  `isKeyField` =  '0' WHERE  `fields`.`id` =225;
